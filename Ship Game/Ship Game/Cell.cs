5a4bd043e727b811d090a57306b2dd05c7693abd
egorikem <egorikem@gmail.com>
2014-01-14 17:58:21 +0400

    Version 0.0.1 alpha

---
+++ Ship Game/Ship Game/Cell.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ship_Game
+{
+    class Cell
+    {
+        private int _x; // X координата 
+        private int _y; // У координата
+        private int _z; // Z координата
+
+        // Конструктор взаимодейстивя с _x
+        public int X
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
+        // Конструктор взаимодейстивя с _y
+        public int Y
+        {
+            get { return _y; }
+            set { _y = value; }
+        }
+        // Конструктор взаимодейстивя с _z 
+        public int Z
+        {
+            get { return _z; }
+            set { _z = value; }
+        }
+    }
+}
